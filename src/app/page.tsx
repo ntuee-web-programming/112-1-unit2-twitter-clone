2c0863a31f9ae9af446a7ecb9c7360aa2fbdfdd0
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2023-10-23 23:16:52 +0800

    fix typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ type HomePageProps = {
 // Since this is a server component, we can do some server side processing
 // in the react component. This may seem crazy at first, but it's actually
 // a very powerful feature. It allows us to do the data fetching and rendering
-// the page in the same place. It may seem wierd to run react code on the server,
+// the page in the same place. It may seem weird to run react code on the server,
 // but remember, react is not just for the browser, react-dom is. React is just
 // the shadow dom and state update logic. We can use react to render anything,
 // any where. There are already libraries that use react to render to the terminal,
